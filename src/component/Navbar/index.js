973e71f502a2877db1f5aba0af83031d7bff8cbe
Ahmedtarekelhelw <elhelwahmed4@gmail.com>
2022-01-01 06:23:02 +0200

    fix some issues

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -1,27 +1,51 @@
-import React from 'react'
-import "./style.css"
-import {NavLink} from "react-router-dom"
+import React from "react";
+import "./style.css";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <header>
-            <div className="container">
-                <nav>
-                    <div className="logo">
-                        <NavLink to="/">Ultra Profile</NavLink>
-                    </div>
-                    <ul>
-                        <li><NavLink className="link" to="/">home</NavLink></li>
-                        <li><NavLink className="link" to="/work">work</NavLink></li>
-                        <li><NavLink className="link" to="/portfolio">portfolio</NavLink></li>
-                        <li><NavLink className="link" to="/profile">profile</NavLink></li>
-                        <li><NavLink className="link" to="/about">about</NavLink></li>
-                        <li><NavLink className="link" to="/Contact">contact</NavLink></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="container">
+        <nav>
+          <div className="logo">
+            <NavLink to="/">Ultra Profile</NavLink>
+          </div>
+          <ul>
+            <li>
+              <NavLink className="link" to="/react-website/">
+                home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/work">
+                work
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/portfolio">
+                portfolio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/profile">
+                profile
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/about">
+                about
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/Contact">
+                contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
